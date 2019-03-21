d676adcc9c25f161b7cd65fe98e073f44abb1e89
RyanLivingston <32498412+RyanLivingston@users.noreply.github.com>
2019-03-20 21:35:34 -0400

    Initial Commit

---
+++ SmartThermos_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartThermos_iOS
+//
+//  Created by Ryan Livingston on 3/20/19.
+//  Copyright © 2019 Ryan Livingston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
